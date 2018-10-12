9557556b705b7cfac8bc98e468420a2444e796da
sherryshall <sherryshall@foxmail.com>
2018-10-13 00:53:24 +0800

    update

--- LeetCode_22_Generate_Parentheses.py
+++ LeetCode_22_Generate_Parentheses.py
@@ -19,19 +19,32 @@ class Solution(object):
 		:type n: int
 		:rtype: List[str]
 		"""
-		unique_step = ["("*i + ")"*i for i in range(1, n+1)]
-		res = [""] * n
-		for i in range(1, n+1):
-			res[i-1] = [unique_step[i-1]]
-			for j in range(1, i):
-				res[i-1] += [unique_step[j-1] + s for s in res[i-j-1]]
-				res[i-1] += [s1+s2 for s1 in res[j-1] for s2 in res[i-j-1]]
-				for s in res[i-j-1]:
-					res[i-1] += [unique_step[j-1][:j] + s + unique_step[j-1][j:]]
-				#res[i-1] += [s.join("") for s in res[i-j-1]]
-		output = list(set(res[n-1]))
-		output.sort()
-		return output
+		# 暴力去重
+		# unique_step = ["("*i + ")"*i for i in range(1, n+1)]
+		# res = [""] * n
+		# for i in range(1, n+1):
+		# 	res[i-1] = [unique_step[i-1]]
+		# 	for j in range(1, i):
+		# 		res[i-1] += [unique_step[j-1] + s for s in res[i-j-1]]
+		# 		res[i-1] += [s1+s2 for s1 in res[j-1] for s2 in res[i-j-1]]
+		# 		for s in res[i-j-1]:
+		# 			res[i-1] += [unique_step[j-1][:j] + s + unique_step[j-1][j:]]
+		# 		#res[i-1] += [s.join("") for s in res[i-j-1]]
+		# output = list(set(res[n-1]))
+		# output.sort()
+		# return output
+		res = []
+		self.backTrack("", 0, 0, n, res)
+		return res
+	# 回溯法
+	def backTrack(self, str, op, clo, mx, res):
+		if len(str) == 2*mx:
+			res.append(str)
+			return
+		if op < mx:
+			self.backTrack(str+"(", op+1, clo, mx, res)
+		if clo < op:
+			self.backTrack(str+")", op, clo+1, mx, res)
 
 
 
